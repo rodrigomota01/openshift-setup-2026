1e886c1c581b9516f75c086405144309e10ad9b4
rodrigomota01 <rodrigo.mota@atena.io>
2026-04-22 19:24:33 -0300

    feat: add Terraform workflow and backend configuration

---
+++ terraform/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "infra-terraform-state-856467372193-us-east-1-an"
+    key            = "proxmox/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true
+  }
+}
